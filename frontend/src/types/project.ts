b6644fd38230c08418dfbb16b46ac855b682df53
Ignacio-CI <ignacio.cerda.i@gmail.com>
2026-03-09 16:22:14 +0100

    initial frontend

---
+++ frontend/src/types/project.ts
@@ -0,0 +1,14 @@
+export type ProjectStatus = 'production' | 'mvp' | 'development';
+
+export interface Project {
+    id: string;
+    title: string;
+    subtitle: string;
+    content: string;
+    company?: string;
+    companyLink?: string;
+    image: string;
+    status: ProjectStatus;
+    link: string;
+    tags: string[];
+}
